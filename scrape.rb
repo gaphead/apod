7a2cc28c87b4419f464865f16a081845f40fa0ca
Erik Pendleton <erik.codigo@gmail.com>
2020-02-10 23:23:25 -0500

    start scraping

--- scrape.rb
+++ scrape.rb
@@ -1,3 +1,9 @@
 require 'open-uri'
 require 'nokogiri'
 
+url = "https://apod.nasa.gov/apod/ap200209.html"
+
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+# html_doc.search('')
