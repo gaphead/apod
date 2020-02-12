274b5fd1bfa9830781c9ec3603a8ad6c3ab945f2
Erik Pendleton <erik.codigo@gmail.com>
2020-02-12 16:40:42 -0500

    thinking about process

--- scrape.rb
+++ scrape.rb
@@ -1,9 +1,17 @@
 require 'open-uri'
 require 'nokogiri'
 
+# url format is: "ap" + two-digit year + two-digit month + two-digit day
+# first content is /ap950616.html, then /ap950620.html, then daily.
+
+# years =
+
 url = "https://apod.nasa.gov/apod/ap200209.html"
 
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
+
+puts html_doc.search('title')
+
 
 # html_doc.search('')
