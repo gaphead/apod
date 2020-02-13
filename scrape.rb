a5f73f96de971b7bee2e63a38cdb6138156ada7f
Erik Pendleton <erik.codigo@gmail.com>
2020-02-13 11:23:51 -0500

    building blocks

--- scrape.rb
+++ scrape.rb
@@ -4,14 +4,24 @@ require 'nokogiri'
 # url format is: "ap" + two-digit year + two-digit month + two-digit day
 # first content is /ap950616.html, then /ap950620.html, then daily.
 
-# years =
+years = %w[95 96 97 98 99 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20]
+months = %w[01 02 03 04 05 06 07 08 09 10 11 12]
+days = %w[01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31]
 
+# root url
+root = "https://apod.nasa.gov"
+
+# page url e.g.
 url = "https://apod.nasa.gov/apod/ap200209.html"
 
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
 
-puts html_doc.search('title')
+# page title
+html_doc.search('title').text.strip
 
+# returns the url and link text for each <a> within <p>
+html_doc.css('p a').each { |a| puts "#{a.text} - #{a.attribute('href').value}" }
 
-# html_doc.search('')
+# returns <p> text (description of image) on page
+html_doc.css('p').text.strip.gsub(/\n/, ' ').gsub(/\"/, "'").gsub(/\s{2,}/, ' ')
