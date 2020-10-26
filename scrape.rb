303166bb335510bb931801fc81a838aa199a3ed1
Erik Pendleton <erik.codigo@gmail.com>
2020-10-26 18:46:37 -0400

    get image urls

--- scrape.rb
+++ scrape.rb
@@ -1,9 +1,17 @@
 require 'open-uri'
 require 'nokogiri'
 
+base_url = "https://apod.nasa.gov/apod"
+
 url = "https://apod.nasa.gov/apod/ap200209.html"
 
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
 
 # html_doc.search('')
+
+# pp html_doc
+
+html_doc.search('img').each do |element|
+  puts "#{base_url}/#{element.attributes['src']}"
+end
